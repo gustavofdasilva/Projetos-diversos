0f133d9e398ecd656f7668ec041f2dfe13758302
Gustavo F. Silva <108156436+gustavofdasilva@users.noreply.github.com>
2022-11-14 13:53:05 -0300

    terminando interatividade com js

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
+
 let landingPages = [];
 let login = [];
 let projectSection = document.getElementById("project-section");
 
 
-
-
-function show(String) {
-    window.alert("oi");
     
+
+function show(type) {
+    for (let i = 0 ; i < projectSection.children.length ; i++) {
+        child = projectSection.children[i];
+        if(type == 'page') {
+            if(child.className.includes('landing-page')) {
+                child.style.display = 'flex';
+            } else {child.style.display = 'none';}
+        } else if (type == 'login') {
+            if(child.className.includes('login')) {
+                child.style.display = 'flex';
+            } else {child.style.display = 'none';}
+        } else {
+            child.style.display = 'flex';
+        }
+    }
 }
